0992723d57d6bd8605d8db6c2ed1528fd879b8ba
adamreaksmey <adaminiature@gmail.com>
2025-01-19 19:34:21 +0700

    replaced eventUser with eventUserController

--- app/Http/Controllers/Items/EventUserController.php
+++ app/Http/Controllers/Items/EventUserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Items;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class EventUser extends Controller
+class EventUserController extends Controller
 {
     /**
      * Display a listing of the resource.
